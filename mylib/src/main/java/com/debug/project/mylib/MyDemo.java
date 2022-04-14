2c4cbb47531c33dbb73355900ca71087923064ce
孔鑫 <xin.kong@chivox.com>
2022-04-14 18:57:15 +0800

    update config

--- mylib/src/main/java/com/debug/project/mylib/MyDemo.java
+++ mylib/src/main/java/com/debug/project/mylib/MyDemo.java
@@ -1,8 +1,13 @@
 package com.debug.project.mylib;
 
+import androidx.annotation.Nullable;
+
 /**
  * 这是一个Gradle Maven的Demo程序
  */
 public class MyDemo {
-
+    @Nullable
+    public String hello(){
+        return "hello";
+    }
 }
